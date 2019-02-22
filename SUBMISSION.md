d863d8b3d325b08d3c6472c5daaf778d1e8b962a
subhamagrawal7 <subhamagrawal7@gmail.com>
2019-02-22 12:56:36 +0530

    Initial changes to nav-menu

--- SUBMISSION.md
+++ SUBMISSION.md
@@ -2,7 +2,10 @@
 
 Use this md to tell us the bugs you solved and also explain the approach you had while solving them. Make them bulleted like
 
-1. Accessibility - **Something here**
+1. Accessibility - \* Changed the icon-menu using class as icon-menu instead of icon-list and also changed the .icon-menu:before content using the icomoon app
+
 2. Aesthetics - **Something here**
 3. Testing - **Something here**
-4. Technicality Improvements - **Something here**
+4. Technicality Improvements - \* Changed the links in the nav menu,refer Handlebars docs
+
+- When the menu icon is clicked on, we need to toggle a class on the body to perform the hiding/showing of our menu.
--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-var allFeeds = [{
-  name: 'CSS Tricks',
-  url: 'http://feeds.feedburner.com/CssTricks'
-}, {
-  name: 'HTML5 Rocks',
-  url: 'http://feeds.feedburner.com/html5rocks'
-}, {
-  name: 'Linear Digressions',
-  url: 'http://feeds.feedburner.com/udacity-linear-digressions'
-}];
+var allFeeds = [
+  {
+    name: "CSS Tricks",
+    url: "http://feeds.feedburner.com/CssTricks"
+  },
+  {
+    name: "HTML5 Rocks",
+    url: "http://feeds.feedburner.com/html5rocks"
+  },
+  {
+    name: "Linear Digressions",
+    url: "http://feeds.feedburner.com/udacity-linear-digressions"
+  }
+];
 
 function init() {
   loadFeed(0);
-  $('body').removeClass('menu-hidden');
+  $("body").removeClass("menu-hidden");
 }
 
 /* This function performs everything necessary to load a
@@ -20,19 +24,18 @@ function init() {
  * which will be called after everything has run successfully.
  */
 function loadFeed(id, cb) {
-
   $.ajax({
     type: "GET",
-    url: 'https://api.rss2json.com/v1/api.json',
+    url: "https://api.rss2json.com/v1/api.json",
     data: {
-      rss_url: 'http://feeds.feedburner.com/CssTricks'
+      rss_url: "http://feeds.feedburner.com/CssTricks"
     },
-    success: function (result, status) {
+    success: function(result, status) {
       if (cb) {
-        cb( /*something*/ );
+        cb(/*something*/);
       }
     },
-    error: function (result, status, err) {
+    error: function(result, status, err) {
       //run only the callback without attempting to parse result due to error
       if (cb) {
         cb();
@@ -43,24 +46,29 @@ function loadFeed(id, cb) {
 }
 window.onload = init;
 
-$(function () {
-  var feedList = $('.feed-list'),
-    feedItemTemplate = Handlebars.compile($('.tpl-feed-list-item').html()),
-    menuIcon = $('.menu-icon-link');
+$(
+  (function() {
+    var feedList = $(".feed-list"),
+      feedItemTemplate = Handlebars.compile($(".tpl-feed-list-item").html()),
+      menuIcon = $(".menu-icon-link");
 
+    allFeeds.forEach(function(feed) {
+      console.log(feed.url);
+      feedList.append(feedItemTemplate(feed));
+    });
 
-  allFeeds.forEach(function (feed) {
-    feedList.append(feedItemTemplate(feed));
-  });
+    feedList.on("click", function() {
+      var item = $(this);
+      $("body").addClass(".feed");
+      loadFeed(item.data("id"));
+      return false;
+    });
 
-  feedList.on('click', function () {
-    var item = $(this);
-    $('body').addClass( /*A class here*/ );
-    loadFeed(item.data('id'));
-    return false;
-  });
-
-  /* When the menu icon is clicked on, we need to toggle a class
-   * on the body to perform the hiding/showing of our menu.
-   */
-}());
+    /* When the menu icon is clicked on, we need to toggle a class
+     * on the body to perform the hiding/showing of our menu.
+     */
+    menuIcon.click(function() {
+      $(".slide-menu").toggle("1000");
+    });
+  })()
+);
